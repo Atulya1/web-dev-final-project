be6806986ba2b63ca5ce71f371477f32c143df2b
chetana47 <chetanareddy64@gmail.com>
2023-04-19 00:33:38 -0400

    upcoming trips beautified by Chetana

--- src/wanderer/Register/index.js
+++ src/wanderer/Register/index.js
@@ -1,46 +1,51 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import "./index.css";
-import {loginUser} from "../../services/wanderer-service.js"
+import {registerUser} from "../../services/wanderer-service";
 import {FormGroup, FormControl, InputLabel, Input, Button, styled, Typography} from '@mui/material';
-
+import {checkUserName} from "../../services/wanderer-service";
 
 const initialValue = {
-    firstName: '',
-    lastName: '',
     username: '',
     password: '',
+    firstName: '',
+    lastName: '',
     email: '',
-    phone: ''
+    dob: '',
+    phone_number: ''
 }
 
-const Container = styled(FormGroup)`
-    width: 50%;
-    margin: 5% 0 0 25%;
-    & > div {
-        margin-top: 20px;
-`;
+
 const RegisterComponent = () => {
 
     const [userRegistration, setUserRegistration] = useState(initialValue);
-    const { firstName, lastName, username, password, email, phone } = userRegistration;
+    const { username, password, firstName, lastName, email, dob, phone_number } = userRegistration;
 
     let navigate = useNavigate();
 
-    const onValueChange = (e) => {
+    const onValueChange = async (e) => {
         console.log({[e.target.name]: e.target.value});
-        setUserRegistration({...userRegistration, [e.target.name]: e.target.value})
+        setUserRegistration({...userRegistration, [e.target.name]: e.target.value});
     }
 
-    const registerUser = async() => {
-        // //const response = await loginUser(user);
-        // console.log(response.data);
-        // if(response.data.status == 200) {
-        //     navigate('/tuiter/profile');
-        // } else {
-            window.alert("registered");
-            navigate('/login')
-        //}
+    const registerUserDetails = async() => {
+        console.log(userRegistration);
+        const response = await registerUser(userRegistration);
+        //console.log(response.data);
+        if(response.data.status == 200) {
+            console.log(response.data.message._id);
+            navigate(`/login/`);
+        } else {
+            window.alert("error occured");
+        }
+        // const responseUser = await checkUserName(userRegistration);
+        // if(responseUser.data.status != 200) {
+        //     console.log(response.data.message._id);
+        //     setMessage('Available.');
+        // }
+        // else {
+        //     setMessage('Sorry, this username is not Available.');
+        // }
     }
 
     return(
@@ -50,15 +55,15 @@ const RegisterComponent = () => {
                     <div className="card-group mb-0">
                         <div className="card p-4">
                             <div className="card-body">
-                                <h1>Login</h1>
+                                <h1>Register </h1>
                                 <p className="text-muted">Register your account</p>
                                 <div className="input-group mb-3">
                                     <span className="input-group-addon"><i className="fa fa-user"></i></span>
-                                    <input type="text" className="form-control" placeholder="firstName" onChange={(e) => onValueChange(e)} name='firstName' value={firstName} id="firstName" />
+                                    <input type="text" className="form-control" placeholder="First Name" onChange={(e) => onValueChange(e)} name='firstName' value={firstName} id="firstName" />
                                 </div>
                                 <div className="input-group mb-4">
                                     <span className="input-group-addon"><i className="fa fa-lock"></i></span>
-                                    <input type="text" className="form-control" placeholder="LastName" onChange={(e) => onValueChange(e)} name='lastName' value={lastName} id="lastName"/>
+                                    <input type="text" className="form-control" placeholder="Last Name" onChange={(e) => onValueChange(e)} name='lastName' value={lastName} id="lastName"/>
                                 </div>
                                 <div className="input-group mb-3">
                                     <span className="input-group-addon"><i className="fa fa-user"></i></span>
@@ -70,15 +75,19 @@ const RegisterComponent = () => {
                                 </div>
                                 <div className="input-group mb-3">
                                     <span className="input-group-addon"><i className="fa fa-user"></i></span>
-                                    <input type="text" className="form-control" placeholder="email" onChange={(e) => onValueChange(e)} name='email' value={email} id="email" />
+                                    <input type="text" className="form-control" placeholder="Email" onChange={(e) => onValueChange(e)} name='email' value={email} id="email" />
                                 </div>
                                 <div className="input-group mb-4">
                                     <span className="input-group-addon"><i className="fa fa-lock"></i></span>
-                                    <input type="text" className="form-control" placeholder="phone" onChange={(e) => onValueChange(e)} name='phone' value={phone} id="phone"/>
+                                    <input type="text" className="form-control" placeholder="Phone" onChange={(e) => onValueChange(e)} name='phone_number' value={phone_number} id="phone_number"/>
+                                </div>
+                                <div className="input-group mb-4">
+                                    <span className="input-group-addon"><i className="fa fa-lock"></i></span>
+                                    <input type="date" className="form-control" placeholder="Date of Birth" onChange={(e) => onValueChange(e)} name='dob' value={dob} id="dob"/>
                                 </div>
                                 <div className="row">
                                     <div className="col-6">
-                                        <button type="button" className="btn btn-primary px-4" onClick={() => registerUser()}>Register</button>
+                                        <button type="button" className="btn btn-primary px-4" onClick={() => registerUserDetails()}>Register</button>
                                     </div>
                                 </div>
                             </div>
